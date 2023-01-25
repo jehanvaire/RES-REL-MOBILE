936f761a1afa306bfb9521130b236637556b36ba
Adrien <adrien.bony38@gmail.com>
2023-01-25 22:22:47 +0100

    ADD: librairie native-base

--- App.tsx
+++ App.tsx
@@ -1,22 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Button from "./components/Button";
-import Screen from "./components/Screen";
+import React from "react";
+// 1. import `NativeBaseProvider` component
+import { NativeBaseProvider, Text, Box } from "native-base";
 
 export default function App() {
+  // 2. Use at the root of your app
   return (
-    <View style={styles.container}>
-      <Screen></Screen>
-      <Button></Button>
-    </View>
+    <NativeBaseProvider>
+      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+        <Text>Open up App.js to start working on your app!</Text>
+      </Box>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
