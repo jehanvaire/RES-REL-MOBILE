288d095529b9f1c47d7d33e6b6782ba0bfb9dcfe
Adrien <adrien.bony38@gmail.com>
2023-03-21 20:37:24 +0100

    FIX: bouton afficher plus dans composant description

--- components/Description.tsx
+++ components/Description.tsx
@@ -1,27 +1,37 @@
 import { Box } from "native-base";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 import { Text, Button, StyleSheet } from "react-native";
 
-export default function Description(props: any) {
-  const [expanded, setExpanded] = useState(false);
+const MAX_LINES = 3;
 
-  const toggleExpand = () => {
-    setExpanded(!expanded);
+export default function Description(props: any) {
+  const [etendu, setEtendu] = useState(false);
+  const [affichePlus, setAffichePlus] = useState(false);
+
+  const switchEtendu = () => {
+    setEtendu(!etendu);
   };
+
+  const affichePlusOuMoins = useCallback((e: any) => {
+    setAffichePlus(e.nativeEvent.lines.length > MAX_LINES);
+  }, []);
 
   return (
     <Box>
-      <Text style={styles.contenu} numberOfLines={expanded ? undefined : 3}>
+      <Text
+        style={styles.contenu}
+        numberOfLines={etendu ? undefined : MAX_LINES}
+        onTextLayout={affichePlusOuMoins}
+      >
         {props.contenu}
       </Text>
-      {/* show the button only if the text is longer than 100 caracters */}
-      {props.contenu.length > 100 && (
+      {affichePlus ? (
         <Button
-          title={expanded ? "Afficher moins" : "Afficher plus"}
-          onPress={toggleExpand}
+          title={etendu ? "Afficher moins" : "Afficher plus"}
+          onPress={switchEtendu}
         />
-      )}
+      ) : null}
     </Box>
   );
 }
