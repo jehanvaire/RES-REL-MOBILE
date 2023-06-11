136a95c3e7a83b2e2a6cdbf33cb17c4b742e100c
Seizuma <82123001+Seizuma@users.noreply.github.com>
2023-06-11 19:09:20 +0200

    Revert "WIP: delete account"
    This reverts commit 9dda5cc20bd782a8e6fd41d271ca02fb30a0ac80.
    
    FIX : JSON EOF

--- src/pages/MenuProfilUtilisateur/ParametresScreen.tsx
+++ src/pages/MenuProfilUtilisateur/ParametresScreen.tsx
@@ -1,61 +1,17 @@
 import { useNavigation } from "@react-navigation/native";
-import { useEffect, useLayoutEffect, useState } from "react";
-import { View, Text, StyleSheet, Alert } from "react-native";
+import { useLayoutEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import Ionicons from "react-native-vector-icons/Ionicons";
-import { AuthentificationEnum } from "../../ressources/enums/AuthentificationEnum";
-import { UtilisateurEntity } from "../../ressources/models/UtilisateurEntity";
-import { storage } from "../../services/AuthentificationService";
-import RestClient from "../../services/RestClient";
-import { useAuth } from "../../services/AuthentificationService";
 
 const ParametresScreen = (props: any) => {
 
   const navigation = useNavigation();
-  const [utilisateur, setUtilisateur] = useState<UtilisateurEntity>({} as UtilisateurEntity);
-  const deleteAPIUrl = "utilisateurs/";
-  const [token, setToken] = useState<string>("");
-  const restClient = new RestClient();
-  const { logout } = useAuth();
   useLayoutEffect(() => {
     navigation.setOptions({
       title: 'Paramètres',
     });
   }, [navigation]);
-
-  useEffect(() => {
-    const utilisateurJson = storage.getString(AuthentificationEnum.CURRENT_USER) ?? "";
-    const utilisateurObject = JSON.parse(utilisateurJson) as UtilisateurEntity;
-    setUtilisateur(utilisateurObject);
-
-    const token = storage.getString(AuthentificationEnum.ACCESS_TOKEN_KEY) ?? "";
-    setToken(token);
-  }, []
-  );
-
-  const handleDeleteAccount = () => {
-    // Show a confirmation prompt before deleting the account
-    Alert.alert(
-      "Supprimer mon compte",
-      "Êtes-vous sûr de vouloir supprimer votre compte? Cette action est irréversible.",
-      [
-        { text: "Annuler", style: "cancel" },
-        { text: "Supprimer", onPress: () => deleteAccount() },
-      ],
-      { cancelable: false }
-    );
-  };
-
-  const deleteAccount = () => {
-    const userID = utilisateur.id;
-    restClient.delete(deleteAPIUrl + userID)
-      .then((response) => {
-        if (response.status === 200) {
-          logout();
-        }
-      }
-      )
-  };
 
   return (
     <View style={styles.container}>
@@ -90,15 +46,6 @@ const ParametresScreen = (props: any) => {
         <Ionicons name={"chevron-forward-outline"} size={30} />
       </TouchableOpacity>
 
-      {/* Delete account */}
-      <TouchableOpacity
-        onPress={handleDeleteAccount}
-        style={styles.parameterItem}
-      >
-        <Ionicons name={"trash-outline"} size={30} />
-        <Text style={styles.parameterText}>Supprimer mon compte</Text>
-        <Ionicons name={"chevron-forward-outline"} size={30} />
-      </TouchableOpacity>
 
     </View>
   );
