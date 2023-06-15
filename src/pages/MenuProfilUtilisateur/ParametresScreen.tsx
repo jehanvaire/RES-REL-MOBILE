59af44a4ced9bb1ea1d53500a148e1c330becc1c
Seizuma <82123001+Seizuma@users.noreply.github.com>
2023-06-15 21:44:18 +0200

    ADD: delete account

--- src/pages/MenuProfilUtilisateur/ParametresScreen.tsx
+++ src/pages/MenuProfilUtilisateur/ParametresScreen.tsx
@@ -1,8 +1,13 @@
 import { useNavigation } from "@react-navigation/native";
-import { useLayoutEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { useEffect, useLayoutEffect, useState } from "react";
+import { View, Text, StyleSheet, Alert } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import { useAuth } from "../../services/AuthentificationService";
+import RestClient from "../../services/RestClient";
+import { UtilisateurEntity } from "../../ressources/models/UtilisateurEntity";
+import { storage } from "../../services/AuthentificationService";
+import { AuthentificationEnum } from "../../ressources/enums/AuthentificationEnum";
 
 const ParametresScreen = (props: any) => {
   const navigation = useNavigation();
@@ -11,6 +16,40 @@ const ParametresScreen = (props: any) => {
       title: "Paramètres",
     });
   }, [navigation]);
+
+  const { logout } = useAuth();
+  const restClient = new RestClient();
+  const [utilisateur, setUtilisateur] = useState<UtilisateurEntity>({} as UtilisateurEntity);
+  const userID = utilisateur.id;
+
+  useEffect(() => {
+    const utilisateurJson = storage.getString(AuthentificationEnum.CURRENT_USER) ?? "";
+    const utilisateurObject = JSON.parse(utilisateurJson) as UtilisateurEntity;
+    setUtilisateur(utilisateurObject);
+  }, []);
+
+  const supprimerCompte = () => {
+    Alert.alert(
+      "Supprimer le compte",
+      "Êtes-vous sûr de vouloir supprimer votre compte ?",
+      [
+        {
+          text: "Annuler",
+          onPress: () => console.log("Annulation de la suppression du compte"),
+          style: "cancel",
+        },
+        {
+          text: "Supprimer",
+          onPress: () => {
+            restClient.delete("utilisateurs/" + userID)
+            logout();
+          },
+          style: "destructive",
+        },
+      ],
+      { cancelable: false }
+    );
+  };
 
   return (
     <View style={styles.container}>
@@ -32,6 +71,13 @@ const ParametresScreen = (props: any) => {
       >
         <Ionicons name={"information-circle-outline"} size={30} />
         <Text style={styles.parameterText}>A propos</Text>
+        <Ionicons name={"chevron-forward-outline"} size={30} />
+      </TouchableOpacity>
+
+      {/* delete my account */}
+      <TouchableOpacity onPress={supprimerCompte} style={styles.parameterItem}>
+        <Ionicons name={"trash-outline"} size={30} />
+        <Text style={styles.parameterText}>Supprimer mon compte</Text>
         <Ionicons name={"chevron-forward-outline"} size={30} />
       </TouchableOpacity>
     </View>
